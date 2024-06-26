d2c8fc55dfc694a4b205e92d361aa5569f3b970d
katajima <k023g1078@g.neec.ac.jp>
2024-06-26 20:09:38 +0900

    discard完成

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -39,12 +39,21 @@ PixelShaderOutput main(VertexShaderOutput input)
     {
         float NdotL = dot(normalize(input.nomal), -gDirectionalLight.direction);
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-        
+            
         //float cos = saturate(dot(normalize(input.nomal), -gDirectionalLight.direction));
         //output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
         output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
         output.color.a = gMaterial.color.a * textureColor.a;
 
+        if (textureColor.a <= 0.5f)
+        {
+            discard;
+        }
+        if (output.color.a <= 0.0f)
+        {
+            discard;
+        }
+        
     }
     else // Lighting‚µ‚È‚¢ê‡B‘O‰ñ‚Ü‚Å‚Æ“¯‚¶‰‰ŽZ
     {

3c09793cead8541e5469dc983b3a0388fe002464
katajima <k023g1078@g.neec.ac.jp>
2024-07-26 15:36:26 +0900

    一旦

--- BasicGeometryShader.hlsl
+++ BasicGeometryShader.hlsl
@@ -1,16 +1,17 @@
 #include"BasicShaderHeader.hlsli" 
 [maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3],// : SV_POSITION,
-	inout TriangleStream<GSOutput> output
+    triangle VSOutput input[3], // 入力の三角形
+    inout TriangleStream<GSOutput> output // 出力のトライアングルストリーム
 )
 {
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos;
-        element.normal = input[i].normal;
-        element.uv = input[i].uv;
-		output.Append(element);
-	}
+    for (uint i = 0; i < 3; i++)
+    {
+        GSOutput element;
+        element.svpos = input[i].svpos; // 入力から位置を取得
+        element.normal = input[i].normal; // 入力から法線を取得
+        element.uv = input[i].uv; // 入力からUVを取得
+        output.Append(element); // 頂点を追加
+    }
+    output.RestartStrip(); // 必要に応じてストリップを再起動
 }
--- BasicShaderHeader.hlsli
+++ BasicShaderHeader.hlsli
@@ -1,3 +1,11 @@
+// 定数バッファの構造体の定義
+cbuffer ConstantBuffer : register(b0) // b0 レジスタにバインド
+{
+    float4x4 WorldViewProjection; // ワールドビュー投影行列
+    float4 LightDirection; // 光源の方向
+    float4 LightColor; // 光源の色
+    float4 AmbientColor; // 環境光の色
+};
 struct GSOutput
 {
     float4 svpos : SV_POSITION;
